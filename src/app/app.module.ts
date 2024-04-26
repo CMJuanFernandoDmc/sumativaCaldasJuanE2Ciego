4403c7e00b8a096f2e4b8b0941b9c51654db397c
CMJuanFernandoDmc <151117518+CMJuanFernandoDmc@users.noreply.github.com>
2024-04-26 11:56:26 -0500

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getDatabase, provideDatabase} from '@angular/fire/database';
+import { environment } from '../environments/environment'; // Importa el entorno
+
+
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
