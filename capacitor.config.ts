c648e4f17e227b04ce02ad8d0cd40a8244d55152
CMJuanFernandoDmc <151117518+CMJuanFernandoDmc@users.noreply.github.com>
2024-04-29 19:00:49 -0500

    commit 5

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,14 +2,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Dia/Noche',
+  appName: 'Luz',
   webDir: 'www',
-
   plugins: {
     LocalNotifications: {
-      smallIcon: "ic_stat_icon_config_sample",//Icono de la app solo para Android
-      iconColor: "#488AFF",//Color de Icono
-      sound: "beep.wav",//Sonido de la notificacion solo para Android
+      smallIcon: "src/assets/icon/termometro.png",//Icono de la app solo para Android
+      iconColor: "#ff8000",//Color de Icono
+      sound: "src/assets/sonido/sonido1.wav",//Sonido de la notificacion solo para Android
     },
   },
 };
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -24,27 +24,43 @@ export class HomePage implements OnInit, OnDestroy{
     }
   }
 
-  async ngOnInit(){
-    await LocalNotifications.requestPermissions();//solicitar permisos de la app
-    await LocalNotifications.schedule({//Elaboracion del objeto notificacion
-      notifications: [
-        {
-          title: "Esta es una notificación emergente",
-          body: "Esta notificación debería ejecutarse en segundo plano pero no cuando lapp esté cerrada",
-          id: 1
-        }
-      ]
-    });
-
-    const route = ref(this.database, "/temperatura/intensidad");
-    this.firebaseSubscription = onValue(route, snapshot => {
-      const temperatureFromFirebase = snapshot.val();
-      if (temperatureFromFirebase !== null && !isNaN(temperatureFromFirebase)) {
-        this.temperature = temperatureFromFirebase;
-        this.updateMercuryHeight();
+async ngOnInit() {
+  const route = ref(this.database, "/temperatura/intensidad");
+  this.firebaseSubscription = onValue(route, async snapshot => {
+    const temperatureFromFirebase = snapshot.val();
+    if (temperatureFromFirebase !== null && !isNaN(temperatureFromFirebase)) {
+      this.temperature = temperatureFromFirebase;
+      this.updateMercuryHeight();
+      if (this.temperature === 0 || this.temperature === 100 || this.temperature === 50) {
+        const notificationTitle = this.temperature === 0 ? "Mucho frío" : "Mucho calor";
+        const notificationBody = this.temperature === 0 ? "Está haciendo mucho frío" : "Está haciendo mucho calor";
+        this.showNotification(notificationTitle, notificationBody);
       }
-    });
-  }
+      const mercuryElement = document.querySelector('.mercury');
+      const thermometerElement = document.querySelector('.thermometer');
+      if (mercuryElement && thermometerElement) {
+        mercuryElement.classList.add('change-temperature');
+        thermometerElement.classList.add('change-temperature');
+        mercuryElement.addEventListener('transitionend', () => {
+          mercuryElement.classList.remove('change-temperature');
+          thermometerElement.classList.remove('change-temperature');
+        });
+      }
+    }
+  });
+}
+updateTemperature(event: any) {
+  this.temperature = event.target.value;
+  this.animate = true;
+  setTimeout(() => {
+    this.animate = false;
+  }, 500);
+  const route = ref(this.database, "/temperatura/intensidad");
+  set(route, this.temperature)
+    .then(() => console.log('Temperature updated successfully to Firebase'))
+    .catch(error => console.error('Error updating temperature to Firebase:', error));
+  this.updateMercuryHeight();
+}
 
   updateMercuryHeight() {
     const percentage = this.temperature / 100;
@@ -95,5 +111,17 @@ export class HomePage implements OnInit, OnDestroy{
     return hex.length === 1 ? '0' + hex : hex;
   }
 
+  async showNotification(title: string, body: string) {
+    await LocalNotifications.requestPermissions();
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          title: title,
+          body: body,
+          id: 1
+        }
+      ]
+    });
+  }
   
 }
